f8757948695f3715a03892728a8862a01c2ec1cc
André Freitas <103605932+andrelsfreitas@users.noreply.github.com>
2022-05-16 08:58:53 -0400

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -13,11 +13,29 @@ int mul(int a, int b);
 
 int add(int a, int b)
 {
-   return 0; // TODO: fix
+int resultado = a + b;
+return resultado;
 }
 
 // Exercise 2: implements function 'mul', that multiplies two numbers
 int mul(int a, int b)
 {
-   return 0; // TODO: fix
+int resultado = a * b;
+return resultado;
+}
+
+int main(){
+    int valor1, valor2;
+    cout << "Digite o primeiro valor: ";
+    cin >> valor1;
+    cout << "Digite o segundo valor: ";
+    cin >> valor2;
+
+    cout << "O valor da soma eh: "<<add(valor1,valor2)<<endl;
+
+    cout << "O valor da multiplicado eh: "<<mul(valor1,valor2)<<endl;
+
+    system("pause");
+
+
 }
